82e4739a590101056f284b8342bdf0870fa836ba
ammar299 <ammynice0@gmail.com>
2024-10-17 07:33:52 +0500

    home page design integration some isssue pending

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 
+// Start a new Stimulus application
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false // Set to true for development
+window.Stimulus = application // Expose the application globally
 
 export { application }
+
+console.log("-----------------------------------------------")
